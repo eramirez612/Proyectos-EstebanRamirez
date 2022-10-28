82358d12b30f8c04bb90b6395aa71ac39054f485
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2022-10-28 15:07:57 -0300

    Actualizacion Validaciones 2

--- Django_evaluacion2/reservas_app/forms.py
+++ Django_evaluacion2/reservas_app/forms.py
@@ -4,7 +4,7 @@ from reservas_app.models import reserva
 class FormReservas(forms.ModelForm):
     
     reservante = forms.CharField(min_length=3, max_length=50)
-    telefono = forms.IntegerField(min_value=999999999, max_value=999999999)
+    telefono = forms.IntegerField(min_value=900000000, max_value=999999999)
     fechareserva = forms.DateField()
     horareserva = forms.TimeField()
     numeropersonas = forms.IntegerField(min_value=1, max_value=15)
