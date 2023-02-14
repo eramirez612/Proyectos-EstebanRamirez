ed7744b66c8f11d77ad9b550b095b57ae17c5a47
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-02-14 14:16:09 -0300

    Nuevo Proyecto multitenant
     porque el anterior no queria funcionar

--- multitenant/multitenant/urls.py
+++ multitenant/multitenant/urls.py
@@ -1,4 +1,4 @@
-"""multitenant_project URL Configuration
+"""multitenant URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- multitenant/multitenant/wsgi.py
+++ multitenant/multitenant/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for multitenant_project project.
+WSGI config for multitenant project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multitenant_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multitenant.settings')
 
 application = get_wsgi_application()
