79addc0b7999a9cfc2523e8825060987df32de85
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-02-24 17:32:24 -0300

    Eliminacion Proyecto mal funcional

--- proyecto_contador/tenant_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from tenant_app.models import Empleado
-
-# Register your models here.
-
-@admin.register(Empleado)
-class EmpleadosAdmin(admin.ModelAdmin):
-    list_display = ('rut','nombre','fecha_ingreso','fecha_termino','estado','contrato','metodo_pago',)
-
