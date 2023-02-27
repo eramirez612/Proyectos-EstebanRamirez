3ff140b1011e1a623fc34a46d660ed7eec7c5d8a
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-02-27 16:05:44 -0300

    Segundo Modelo a medias

--- proyecto_contador_2/tenant_app/models.py
+++ proyecto_contador_2/tenant_app/models.py
@@ -26,6 +26,20 @@ payment_choices = [
     ('Transferencia Bancaria', 'Transferencia Bancaria'),
 ]
 
+sex_choices = [
+    ('--Seleccione su genero--', '--Seleccione su genero--'),
+    ('Masculino', 'Masculino'),
+    ('Femenino', 'Femenino'),
+    ('No binario', 'No binario'),
+    ('Prefiero no decirlo', 'Prefiero no decirlo'),
+]
+
+taxes_choices = [
+    ('--Seleccione el tipo de impuesto--', '--Seleccione el tipo de impuesto--'),
+    ('Normal', 'Normal'),
+    ('Trabajador Agricola', 'Trabajador Agricola'),
+]
+
 class Empleado(models.Model):
     rut = models.CharField(max_length=9, primary_key=True)
     nombre = models.CharField(max_length=100)
@@ -50,4 +64,34 @@ class Empleado(models.Model):
         choices=payment_choices,
         default=1
     )
+
+class Datos_Empleado(models.Model):
+    Rut = models.CharField(max_length=9, primary_key=True)
+    Nombres = models.CharField(max_length=100)
+    Apellidos = models.CharField(max_length=100)
+    Direccion = models.CharField(max_length=100)
+    Comuna = models.CharField(max_length=100)
+    Sexo = models.CharField(
+        max_length = 50,
+        null=False, blank=False,
+        choices=sex_choices,
+        default=1
+    )
+    Numero_De_Pasaporte = models.CharField(max_length=10)
+    Labor_en_Liquidacion = models.CharField(max_length=50)
+    Celular = models.IntegerField(max_length=9)
+    Email = models.EmailField()
+    Pensionado_por_Invalidez = models.BooleanField()
+    Profesional = models.BooleanField()
+    Tipo_de_impuesto_unico = models.CharField(
+        max_length = 50,
+        null=False, blank=False,
+        choices=taxes_choices,
+        default=1
+    )
+    Descuento_Prestamo_SII = models.BooleanField()
+
+
+    
+    
 
