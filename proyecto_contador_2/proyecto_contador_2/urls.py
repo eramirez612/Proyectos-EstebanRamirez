3a79ff2308ef983270b948ef668bf1d710224f15
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-03-07 15:18:59 -0300

    se agregaron campos al views

--- proyecto_contador_2/proyecto_contador_2/urls.py
+++ proyecto_contador_2/proyecto_contador_2/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tenant_app.views import views
+from tenant_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('proyecto_contador.urls')),
+    path('', views.index),
 ]
