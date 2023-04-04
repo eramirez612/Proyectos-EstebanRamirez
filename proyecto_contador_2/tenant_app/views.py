4de5f7120d6667cc54bdd9b7362d0e8424cb00ad
marcelo callinir <marcelo_ca_sa@hotmail.cl>
2023-04-04 18:10:58 -0400

    nuevo template y cambios a views

--- proyecto_contador_2/tenant_app/views.py
+++ proyecto_contador_2/tenant_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, Datos_EmpleadoForm
+from .models import Datos_Empleado
 from  django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 # Create your views here.
@@ -7,18 +8,6 @@ from django.contrib.auth import login, logout, authenticate
 @login_required(login_url="/login")
 def home(request):
     return render(request, 'main/home.html')
-
-def sign_up(request):
-    if  request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if  form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/home')
-    else:
-        form = RegisterForm()
-    
-    return render(request, 'registration/sign_up.html', {'form': form})
 
 @login_required(login_url="/login")
 def nuevo_trabajador(request):
@@ -33,3 +22,20 @@ def nuevo_trabajador(request):
         form = Datos_EmpleadoForm()
     
     return render(request, 'main/nuevo_trabajador.html', {"form": form})
+
+def sign_up(request):
+    if  request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if  form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'registration/sign_up.html', {'form': form})
+
+def TrabajadorList(request):
+    trabajador = Datos_Empleado.objects.all
+    data = {'trabajadores': trabajador}
+    return render(request, 'main/trabajadores.html', data)
