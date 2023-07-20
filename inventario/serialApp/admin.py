768df196c38f0cbaaf7435f701391638495f3794
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-07-20 09:30:53 -0400

    Admin Update

--- inventario/serialApp/admin.py
+++ inventario/serialApp/admin.py
@@ -1,7 +1,20 @@
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class RegistroResource(resources.ModelResource):
+    fields = (
+        'id',
+        'nombre',
+        'departamento',
+        'marca_modelo',
+        'serial_number',
+        'tipo_dispositivo')
+    class Meta:
+        model = Registro_Equipo
 
 @admin.register(Registro_Equipo)
 class RegistroAdmin(admin.ModelAdmin):
