f5a2e34b29870c6654aa653cddea9dd787a67b84
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-07-24 11:22:21 -0400

    INVENTARIO (V.1.1)
    Se agrego un filtro de fechas personalizado

--- inventario/serialApp/admin.py
+++ inventario/serialApp/admin.py
@@ -1,3 +1,4 @@
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
@@ -22,5 +23,5 @@ class RegistroAdmin(ImportExportModelAdmin ,admin.ModelAdmin):
     list_display = ('id','nombre', 'departamento', 'marca_modelo', 'serial_number', 'tipo_dispositivo', 'fecha_ingreso_del_registro',)
     ordering = ('-fecha_ingreso_del_registro','departamento',)
     search_fields = ('nombre','departamento','serial_number','fecha_ingreso_del_registro',)
-    list_filter = ('fecha_ingreso_del_registro','departamento','tipo_dispositivo',)
+    list_filter = (('fecha_ingreso_del_registro',DateRangeFilter),'departamento','tipo_dispositivo',)
     list_per_page = 20
