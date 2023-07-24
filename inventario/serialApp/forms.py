dd37df73a7434b6a299417a3ce40d00dc058ee04
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-07-24 11:51:49 -0400

    Date Picker (1.1.1)
    Se agrego un date picker para hacer el ingreso de los datos al inventario

--- inventario/serialApp/forms.py
+++ inventario/serialApp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from serialApp.models import *
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class FormEquipo(forms.ModelForm):
+    
+    fecha_ingreso_del_registro = forms.DateTimeField(widget=DateInput)
 
     class Meta:
         model = Registro_Equipo
