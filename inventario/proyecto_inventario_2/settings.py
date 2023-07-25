f59f03dc50b31c9e2d34601bf7175d2530b80a60
Esteban Ramirez <esteban.ramirezbarriga@gmail.com>
2023-07-25 09:47:01 -0400

    Admin Interface (1.1.3)
    Se agrego la librería admin interface para customizar la UI

--- inventario/proyecto_inventario_2/settings.py
+++ inventario/proyecto_inventario_2/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['192.168.0.8', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
